9358fd0559e950dabc1915ba0e642743f07fff37
scholnicks <steve@scholnick.net>
2013-12-25 16:28:41 -0500

    added PyDoc. Collapsed an if else

--- is-dropbox-current.py
+++ is-dropbox-current.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python -B
 # -*- coding: utf-8 -*-
 
-"""is-drop-current: Checks installed Dropbox version against latest version available.
+"""
+is-drop-current: Checks installed Dropbox version against latest version available.
 
 Inspired by: https://github.com/tjluoma/is-dropbox-current (zsh version)
+
+(c) Steven Scholnick <steve@scholnick.net>
+
+The is-drop-current source code is published under version 2.1 of the GNU Lesser General Public License (LGPL).
+
+In brief, this means there's no warranty and you can do anything you like with it.
+However, if you make changes to rename and redistribute those changes,
+then you must publish your modified version under the LGPL.
 """
 
 from __future__ import print_function
@@ -13,6 +22,7 @@ import feedparser
 STABLE_BUILD = 'New Stable Build: '
 
 def main():
+    '''main method'''
     installed_version = get_installed_version()
 
     if options.debug:
@@ -25,14 +35,14 @@ def main():
 
     if installed_version != latest_version:
         print("{0} is installed. Latest version available is {1}".format(installed_version,latest_version))
-    else:
-        if options.debug:
-            print("Versions match: {0}".format(installed_version))
+    elif options.debug:
+        print("Versions match: {0}".format(installed_version))
 
     sys.exit(0)
 
 
 def get_installed_version():
+    '''Returns the current installed version by parsing Dropbox's client Info.plist'''
     with open('/Applications/Dropbox.app/Contents/Info.plist') as fp:
         plist = [line.strip() for line in fp]
 
@@ -44,6 +54,7 @@ def get_installed_version():
 
 
 def get_latest_version():
+    '''Returns the latest version announced in the release_notes RSS feed'''
     parser = feedparser.parse("https://www.dropbox.com/release_notes/rss.xml")
     for e in parser.entries:
         if e.title.startswith(STABLE_BUILD):
