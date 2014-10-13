6a3fd189a6ee0edb733b2fc2791c5513579d5001
scholnicks <steve@scholnick.net>
2014-10-13 10:32:56 -0400

    initial checkin

---
+++ yuicompress.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+YUICOMPRESSOR_HOME=/opt/yuicompressor
+
+verbose=false
+quiet=false
+
+while getopts "qv" opt; do
+  case $opt in
+    q)
+      quiet=true
+      ;;
+    v)
+      verbose=true
+      ;;
+    \?)
+      exit 1
+      ;;
+  esac
+done
+
+for file in `find . -name "*.js" -o -name "*.css" -print | sort`
+do
+    if [ $verbose == "true" ]; then
+        echo "Compressing $file"
+    elif [ $quiet == "true" ]; then
+        :
+    else
+        echo -n "."
+    fi
+
+    java -jar $YUICOMPRESSOR_HOME/yuicompressor.jar -o $file $file
+done
+
+echo ""
+
+exit 0
