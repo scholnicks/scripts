c3a0f5297dc18174206975e0c05db769856e5e89
scholnicks <steve@scholnick.net>
2015-05-17 12:40:49 -0400

    converted to use docopt. updated help and command line arguments

--- rename.py
+++ rename.py
@@ -4,7 +4,23 @@
 """
 rename:  Renames files in powerful ways
 
-(c) Steven Scholnick <steve@scholnick.net>
+Usage:
+    rename [options] <files> ...
+
+Options:
+    -a, --append=<suffix>                      Suffix to be appended
+    -d, --delimiter=<delimiter>                Specifies the delimiter for fixing numerical filenames
+    -f, --fix=<number of digits to fix>        Fixes numerical file names [default: 4]
+    -h, --help                                 Show this help screen
+    -l, --lower                                Translates the filenames to lowercase
+    -p, --prepend=<prefix>                     Prefix to be prepended
+    -r, --remove=<pattern>                     Pattern to be removed
+    -s, --substitute=<substitution pattern>    Substitutes a pattern (old/new)
+    -t, --test                                 Test mode (Just prints the renames)
+    --version                                  Prints the version
+    -v, --verbose                              Verbose Mode
+
+(c) Steven Scholnick <scholnicks@gmail.com>
 
 The rename source code is published under a MIT license. See http://www.scholnick.net/license.txt for details.
 """
@@ -12,13 +28,10 @@ The rename source code is published under a MIT license. See http://www.scholnic
 from __future__ import print_function
 import os, sys, re
 
-MINIMUM_NUMBER_LENGTH = 4
-
-
 def main(files):
     """Main Method"""
-    if options.verbose:
-        print("Renaming {0}".format(", ".join(files)))
+    if arguments['--verbose']:
+        print("Renaming: {0}".format(", ".join(files)))
 
     for fileName in files:
         renameFile(fileName)
@@ -33,28 +46,29 @@ def renameFile(fileName):
 
     newFileName = fileName
 
-    if options.lower:
+    if arguments['--lower']:
         newFileName = newFileName.lower()
 
-    if options.append:
-        newFileName = newFileName + options.append
+    if arguments['--append']:
+        newFileName = newFileName + arguments['--append']
 
-    if options.prepend:
-        newFileName = options.prepend + newFileName
+    if arguments['--prepend']:
+        newFileName = arguments['--prepend'] + newFileName
 
-    if options.remove:
-        newFileName = re.sub(options.remove,r'',newFileName)
+    if arguments['--remove']:
+        newFileName = re.sub(arguments['--remove'],r'',newFileName)
 
-    if options.fixNumbers:
-        newFileName = fixNumbers(newFileName,options.fixNumbers)
+    if arguments['--delimiter']:
+        newFileName = fixNumbers(newFileName,arguments['--delimiter'],arguments['--fix'])
 
-    if options.substitute:
-        newFileName = substitute(newFileName,options.substitute)
+    if arguments['--substitute']:
+        newFileName = substitute(newFileName,arguments['--substitute'])
 
     if newFileName != fileName:
-        if options.verbose: print("Renaming {0} to {1}".format(fileName,newFileName))
+        if arguments['--verbose']:
+            print("Renaming {0} to {1}".format(fileName,newFileName))
 
-        if not options.test:
+        if not arguments['--test']:
             os.rename(fileName,newFileName)
 
 
@@ -68,8 +82,8 @@ def substitute(fileName,pattern):
         sys.exit(-1)
 
 
-def fixNumbers(fileName,delimiter):
-    """Fixes the nnumeric part of a filename"""
+def fixNumbers(fileName,delimiter,numberLength):
+    """Fixes the numeric part of a filename"""
     if delimiter not in fileName:
         return fileName
 
@@ -79,29 +93,17 @@ def fixNumbers(fileName,delimiter):
     sequenceValue = number
 
     # TODO: rewrite using the * operator for strings
-    for i in xrange(len(number),MINIMUM_NUMBER_LENGTH):
+    for i in xrange(len(number),numberLength):
         sequenceValue = "0" + sequenceValue
 
     return prefix + delimiter + sequenceValue + extension
 
 
 if __name__ == '__main__':
-    import argparse
+    from docopt import docopt
+    arguments = docopt(__doc__, version='1.1.0')
 
-    parser = argparse.ArgumentParser(description='Renames files in powerful ways')
-    parser.add_argument('-a','--append',     dest="append",     action="store",      help='Appends a suffix to each filename')
-    parser.add_argument('-f','--fix-numbers',dest="fixNumbers", action="store",      help='Fix numbers. Ensures that a minimum number of leading zeroes is present')
-    parser.add_argument('-l','--lower',      dest="lower",      action="store_true", help='Translates the filenames to lowercase')
-    parser.add_argument('-p','--prepend',    dest="prepend",    action="store",      help='Prepend a prefix to each filename')
-    parser.add_argument('-r','--remove',     dest="remove",     action="store",      help='Removes a pattern')
-    parser.add_argument('-s','--substitute', dest="substitute", action="store",      help='Substitutes a pattern (old/new)')
-    parser.add_argument('-t','--test',       dest="test",       action="store_true", help='Toggles test mode (pretends to rename only)')
-    parser.add_argument('-v','--verbose',    dest="verbose",    action="store_true", help='Toggles verbose')
-    parser.add_argument('files', nargs="+", type=str)
+    if arguments['--test']:
+        arguments['--verbose'] = True
 
-    options = parser.parse_args()
-
-    if options.test:
-        options.verbose = True
-
-    main(options.files)
+    main(arguments['<files>'])
