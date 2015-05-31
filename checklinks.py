9cee743a4f72914aa7a3596ab16a9782e9f1100f
scholnicks <steve@scholnick.net>
2015-05-31 07:55:33 -0400

    updated to use docopt

--- checklinks.py
+++ checklinks.py
@@ -3,10 +3,23 @@
 
 """checklinks - validate links in one or more HTML files
 
-Requires external modules that can be installed from PyPI with:
-    pip install lxml requests beautifulsoup4 
+Usage:
+    checklinks [options]  <files> ...
 
-(c) Steven Scholnick <steve@scholnick.net>
+Options:
+    -h, --help                    Show this help screen
+    -i, --image                   Turns on image checking
+    -o, --ok                      Shows the good links
+    -r, --root=<root_directory>   Sets the root web directory
+    -v, --verbose                 Verbose Mode
+    --version                     Prints the version
+
+Requires external modules that can be installed from PyPI with:
+    pip install lxml
+    pip install requests
+    pip install beautifulsoup4
+
+(c) Steven Scholnick <scholnicks@gmail.com>
 
 The checklinks source code is published under a MIT license. See http://www.scholnick.net/license.txt for details.
 """
@@ -19,9 +32,9 @@ import requests
 FORMAT="{0:80.70s} {1:10s}"
 
 
-def main():
+def main(html_files):
     try:
-        for filePath in args:
+        for filePath in html_files:
             processFile(filePath)
     except KeyboardInterrupt:
         print()
@@ -31,14 +44,14 @@ def main():
 
 def processFile(filePath):
     """parses an HTML file"""
-    if options.verbose:
+    if arguments['--verbose']:
         print("Processing file {0}".format(filePath))
 
     with open(filePath,'r') as fp:
         soup = BeautifulSoup(fp,'lxml')
         checkLinks(soup.find_all('link'),'href')
         checkLinks(soup.find_all('a'),'href')
-        if options.image:
+        if arguments['--image']:
             checkLinks(soup.find_all('img'),'src')
         checkLinks(soup.find_all('script'),'src')
 
@@ -61,10 +74,10 @@ def checkRemote(link):
     """checks a remote (http/https) link"""
     try:
         request = requests.get(link)
-        if request.status_code != 200 or options.showGood:
+        if request.status_code != 200 or arguments['--ok']:
             print(FORMAT.format(link, str(request.status_code)))
     except requests.exceptions.RequestException as e:
-        print(FORMAT.format(link,e.message if options.verbose else 'Cannot contact host'))
+        print(FORMAT.format(link,e.message if arguments['--verbose'] else 'Cannot contact host'))
 
 
 def checkLocal(path):
@@ -72,34 +85,23 @@ def checkLocal(path):
     if 'mailto' in path or path.startswith("#"):
         return
 
-    if path[0] == '/' and options.rootDirectory:
-        root = options.rootDirectory
+    if path[0] == '/' and arguments['--root']:
+        root = arguments['--root']
         path = (root[:-1] if root[-1] == '/' else root) + path
 
     exists = os.path.exists(path)
-    if not exists or options.showGood:
+    if not exists or arguments['--ok']:
         print(FORMAT.format(path, "Good" if exists else "Missing"))
 
 
 if __name__ == '__main__':
-    from optparse import OptionParser
+    from docopt import docopt
+    arguments = docopt(__doc__, version='1.1.0')
 
-    parser = OptionParser(usage="%prog [options] filenames")
-    parser.add_option('-i','--image',   dest="image",         action="store_true", help='Turns on image checking')
-    parser.add_option('-o','--ok',      dest="showGood",      action="store_true", help='Shows the good links')
-    parser.add_option('-r','--root',    dest="rootDirectory", type='string',       help='Sets the root web directory')
-    parser.add_option('-v','--verbose', dest="verbose",       action="store_true", help='Toggles verbose')
+    if arguments['--verbose']:
+        arguments['--ok'] = True
 
-    options,args = parser.parse_args()
+    if arguments['--root']:
+        arguments['--root'] = os.path.expanduser(arguments['--root'])
 
-    if options.verbose:
-        options.showGood = True
-
-    if len(args) == 0:
-        parser.print_help()
-        sys.exit(1)
-
-    if options.rootDirectory:
-        options.rootDirectory = os.path.expanduser(options.rootDirectory)
-
-    main()
+    main(arguments['<files>'])
