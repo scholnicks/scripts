b0cc9786fbbd981b3c24178d04d65e7706319d65
scholnicks <scholnicks@gmail.com>
2017-12-23 13:34:06 -0500

    simplified command calls and docs

--- music-format-change.py
+++ music-format-change.py
@@ -9,8 +9,8 @@ Usage:
 
 Options:
     -a, --aiff          Convert to AIFF (default)
-    --album=<album>     Album
-    --artist=<artist>   Artist
+    --album=<album>     MP3 Album Tag Info
+    --artist=<artist>   MP3 Artist Tag Info
     -d, --delete        Delete the input file after conversion
     -h, --help          Show this help screen
     -3, --mp3           Convert to MP3
@@ -27,20 +27,19 @@ import sys
 import os
 import re
 
+
 ENCODERS = {
     'aiff': '/usr/local/bin/sox -q',
     'mp3':  '/usr/local/bin/lame --nohist --silent',
     'wav':  '/usr/local/bin/sox -q'
 }
 
-VERBOSE_MODE = '{0} "{1}" "{2}"'
-QUIET_MODE   = '{0} "{1}" "{2}" 1>/dev/null 2>&1'
+CONVERT_COMMAND_LINE = '{0} "{1}" "{2}" 1>/dev/null 2>&1'
+EYED3_COMMAND_LINE   = 'eyed3 --quiet --title "{}" --album "{}" --artist "{}" --album-artist "{}" --track {} --track-total {} "{}" 1>/dev/null 2>&1'
 
-EYED3_COMMAND_LINE = 'eyed3 --quiet --title "{}" --album "{}" --artist "{}" --album-artist "{}" --track {} --track-total {} "{}" 1>/dev/null 2>&1'
 
 def main(files):
     '''Main method'''
-
     convertableFiles = [f for f in files if f.endswith(tuple(ENCODERS.keys()))]
     for index,musicFile in enumerate(convertableFiles,1):
         destinationFile = convertFile(musicFile)
@@ -51,6 +50,7 @@ def main(files):
 
 
 def filename(filenameWithExtension):
+    '''Returns the filename without the extension'''
     return os.path.splitext(filenameWithExtension)[0]
 
 
@@ -61,8 +61,7 @@ def convertFile(musicFile):
     if not arguments['--quiet']:
         print(f"Converting {musicFile} to {destinationFilename}")
 
-    command = QUIET_MODE if arguments['--quiet'] else VERBOSE_MODE
-    os.system(command.format(ENCODERS[extension],musicFile,destinationFilename))
+    os.system(CONVERT_COMMAND_LINE.format(ENCODERS[extension],musicFile,destinationFilename))
 
     if arguments['--delete']:
         os.unlink(musicFile)
@@ -71,17 +70,19 @@ def convertFile(musicFile):
 
 
 def addMP3Tags(destinationFile,index,numberOfFiles):
+    '''Adds the MP3 Tags using the eyeD3 command line tool'''
     match = re.match(r'^[0-9]+ - (.*)\.mp3$',destinationFile)
 
     os.system(EYED3_COMMAND_LINE.format(
         match.groups()[0] if match else filename(destinationFile),
-        arguments['--album'],
-        arguments['--artist'],
-        arguments['--artist'],
+        arguments['--album'] if arguments['--album'] else '',
+        arguments['--artist'] if arguments['--artist'] else '',
+        arguments['--artist'] if arguments['--artist'] else '',
         index,
         numberOfFiles,
         destinationFile
     ))
+
 
 def getDestinationFormat():
     '''Returns the destination format based on the command line arguments'''
