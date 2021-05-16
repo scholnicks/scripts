a755e6802dc8c5372d8f7146079889a4d839cc55
Steve Scholnick <scholnicks@gmail.com>
2021-05-16 12:19:20 -0400

    replacement patter is now an option

--- far.py
+++ far.py
@@ -8,45 +8,39 @@
 far: Find and replace all instances of a string with a new string in a directory and all its sub-directories.
 
 Usage:
-    far [--verbose] <extension> <substitution-pattern> [<starting-directory>]
+    far [options] [<extension>] [<pattern>] [<starting-directory>]
 
 Options:
-    -h, --help     Show this help screen
-    -v, --verbose  Verbose Mode
-    --version      Prints the version
+    -h, --help                Show this help screen
+    -r,--replacement=<text>   Replacement text
+    --usage                   Detailed usage information
+    -v, --verbose             Verbose Mode
+    --version                 Prints the version
 
 if <starting-directory> is not specified, the current working directory is used.
-
-Example
--------
-far .html 'foo/bar' .
-
-For all files starting with the directory website, all foo references are changed to bar.
-
-Similar to find . -name "*.html" -print0 | xargs -0 sed -i '' -e 's/foo/bar/g'
 """
 
 import os, sys, re
 
 def main(startingDirectory):
+    if arguments['--usage'] or not startingDirectory or not arguments['<extension>'] or not arguments['<pattern>']:
+        usage()
+
     eligible_files = []
     for root, dirs, files in os.walk(os.path.abspath(startingDirectory if startingDirectory else '.')):
         eligible_files += [os.path.join(root,f) for f in files if f.lower().endswith(arguments['<extension>'])]
 
-    try:
-        for file in eligible_files:
-            processFile(file)
-            if arguments['--verbose']:
-                print("Processed {0}".format(file))
-    except AttributeError:
-        print("far: Illegal substitute pattern. Pattern must be old/new",file=sys.stderr)
-        sys.exit(-1)
+    for file in eligible_files:
+        processFile(file)
+        if arguments['--verbose']:
+            print("Processed {0}".format(file))
 
     sys.exit(0)
 
 
 def processFile(file):
-    (old,new) = re.match(r'^(.*)/(.*)$',arguments['<substitution-pattern>']).groups()
+    old = arguments['<pattern>']
+    new = arguments['--replacement'] if arguments['--replacement'] else ''
 
     with open(file,"r") as inFile:
         input_data = inFile.readlines()
@@ -57,7 +51,21 @@ def processFile(file):
         outFile.writelines(output)
 
 
+def usage():
+    print(__doc__ + """
+Example
+-------
+far .html --replacement "bar" foo .
+
+For all files starting with the directory website, all foo references are changed to bar.
+
+Similar to find . -name "*.html" -print0 | xargs -0 sed -i '' -e 's/foo/bar/g'
+
+""")
+    sys.exit(0)
+
+
 if __name__ == '__main__':
     from docopt import docopt
-    arguments = docopt(__doc__, version='1.0.1')
+    arguments = docopt(__doc__, version='1.1.0')
     main(arguments['<starting-directory>'])
