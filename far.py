ac47a2cab54e2c3bc258c8bee570b71ec67f9e3b
Steve Scholnick <scholnicks@gmail.com>
2021-05-21 10:13:30 -0400

    Cleaned up extensions

--- far.py
+++ far.py
@@ -8,39 +8,44 @@
 far: Find and replace all instances of a string with a new string in a directory and all its sub-directories.
 
 Usage:
-    far [options] [<extension>] [<pattern>] [<starting-directory>]
+    far [options] [<replacement>] [<pattern>]
 
 Options:
-    -h, --help                Show this help screen
-    -r,--replacement=<text>   Replacement text
-    --usage                   Detailed usage information
-    -v, --verbose             Verbose Mode
-    --version                 Prints the version
-
-if <starting-directory> is not specified, the current working directory is used.
+    -d, --directory=<directory>   Starting directory [default: .]
+    --dry-run                     Just list the files to be changed, no actual changes
+    -e, --extension=<extension>   File extension
+    -h, --help                    Show this help screen
+    --usage                       Detailed usage information
+    -v, --verbose                 Verbose Mode
+    --version                     Prints the version
 """
 
-import os, sys, re
+import os
+import sys
+import re
 
-def main(startingDirectory):
-    if arguments['--usage'] or not startingDirectory or not arguments['<extension>'] or not arguments['<pattern>']:
+def main():
+    if arguments['--usage'] or not arguments['<pattern>'] or not arguments['<replacement>']:
         usage()
 
-    eligible_files = []
-    for root, dirs, files in os.walk(os.path.abspath(startingDirectory if startingDirectory else '.')):
-        eligible_files += [os.path.join(root,f) for f in files if f.lower().endswith(arguments['<extension>'])]
+    if arguments['--dry-run']:
+        arguments['--verbose'] = True
 
-    for file in eligible_files:
-        processFile(file)
-        if arguments['--verbose']:
-            print("Processed {0}".format(file))
+    eligibleFiles = []
+    for root, dirs, files in os.walk(os.path.abspath(arguments['--directory'])):
+        eligibleFiles += [os.path.join(root,f) for f in files if matches(f)]
+
+    for file in eligibleFiles:
+        if not arguments['--dry-run']:
+            processFile(file)
+        if arguments['--verbose']: print("Processed {0}".format(file))
 
     sys.exit(0)
 
 
 def processFile(file):
     old = arguments['<pattern>']
-    new = arguments['--replacement'] if arguments['--replacement'] else ''
+    new = arguments['<replacement>']
 
     with open(file,"r") as inFile:
         input_data = inFile.readlines()
@@ -51,11 +56,15 @@ def processFile(file):
         outFile.writelines(output)
 
 
+def matches(filename):
+    return filename.lower().endswith(arguments['--extension']) if arguments['--extension'] else True
+
+
 def usage():
     print(__doc__ + """
 Example
 -------
-far .html --replacement "bar" foo .
+far -e .html bar foo
 
 For all files starting with the directory website, all foo references are changed to bar.
 
@@ -67,5 +76,5 @@ Similar to find . -name "*.html" -print0 | xargs -0 sed -i '' -e 's/foo/bar/g'
 
 if __name__ == '__main__':
     from docopt import docopt
-    arguments = docopt(__doc__, version='1.1.0')
-    main(arguments['<starting-directory>'])
+    arguments = docopt(__doc__, version='1.1.1')
+    main()
